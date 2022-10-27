87a6a8521ca68833940f6fafd69bb8e3a2543f8a
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-27 11:49:57 +0900

    release

--- data-platform-local-region-sql-local-region-data.sql
+++ data-platform-local-region-sql-local-region-data.sql
@@ -1,7 +1,12 @@
 CREATE TABLE `data_platform_local_region_local_region_data`
 (
-  `LocalRegion`              varchar(2) NOT NULL,
-  `Country`                  varchar(2) NOT NULL,
-    PRIMARY KEY (`LocalRegion`, `Country`)
+  `LocalRegion`              varchar(3) NOT NULL,
+  `Country`                  varchar(3) NOT NULL,
+  `GlobalRegion`             varchar(3) NOT NULL,
+
+    PRIMARY KEY (`LocalRegion`, `Country`),
+
+    CONSTRAINT `DataPlatformLocalRegionLocalRegionDataCountry_fk` FOREIGN KEY (`Country`, `GlobalRegion`) REFERENCES `data_platform_country_country_data` (`Country`, `GlobalRegion`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-local-region-sql-local-region-text-data.sql
+++ data-platform-local-region-sql-local-region-text-data.sql
@@ -1,10 +1,13 @@
 CREATE TABLE `data_platform_local_region_local_region_text_data`
 (
-  `LocalRegion`              varchar(2) NOT NULL,
-  `Country`                  varchar(2) NOT NULL,
-  `Language`                 varchar(3) NOT NULL,
+  `LocalRegion`              varchar(3) NOT NULL,      
+  `Language`                 varchar(2) NOT NULL,
   `LocalRegionName`          varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`LocalRegion`, `Country`, `Language`),
-    CONSTRAINT `DataPlatformLocalRegionLocalRegionTextData_fk` FOREIGN KEY (`LocalRegion`, `Country`) REFERENCES `data_platform_local_region_local_region_data` (`LocalRegion`, `Country`)
+  
+    PRIMARY KEY (`LocalRegion`, `Language`),
+    
+    CONSTRAINT `DataPlatformLocalRegionLocalRegionTextData_fk` FOREIGN KEY (`LocalRegion`) REFERENCES `data_platform_local_region_local_region_data` (`LocalRegion`),
+    CONSTRAINT `DataPlatformLocalRegionLocalRegionTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4;
